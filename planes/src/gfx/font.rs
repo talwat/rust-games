714cc05172bbfb7444e68525af2cdee0f288d617
talwat <83217276+talwat@users.noreply.github.com>
2023-10-12 17:41:13 +0200

    feat: add more stuff

---
+++ planes/src/gfx/font.rs
@@ -0,0 +1,96 @@
+//! Deals With the... psf2 format??? Jeez, this is really random.
+//! The psfu format is what's used in the linux tty.
+//! You can find the built in psf2 fonts in /usr/share/kbd/consolefonts.
+//! 
+//! This doesn't support the original psf, and currently doesn't support glyphs that aren't 8px wide.
+
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum FontError {
+    IncorrectHeader
+}
+
+impl fmt::Display for FontError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            FontError::IncorrectHeader => write!(fmt, "header magic does not match, is this a psf2 font?"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Header {
+    pub magic: [u8; 4],
+    pub version: u32,
+    pub size: u32,
+    pub flags: u32,
+    pub length: u32,
+    pub glyph_size: u32,
+    pub glyph_height: u32,
+    pub glyph_width: u32,
+}
+
+#[derive(Debug)]
+pub struct Font {
+    pub header: Header,
+    char_data: Box<[u8]>,
+}
+
+impl Font {
+    pub fn get_char(&self, char: char) -> Vec<Vec<u8>> {
+        let char = char as u32 - 3;
+
+        let mut result: Vec<Vec<u8>> = Vec::with_capacity(self.header.glyph_height as usize);
+
+        let from = self.header.size + self.header.glyph_size * (char);
+        let to = self.header.size + self.header.glyph_size * (char+1);
+
+        for (i, byte) in self.char_data[from as usize..to as usize].iter().enumerate() {
+            if byte == &0 {
+                result.push(Vec::with_capacity(0));
+
+                continue;
+            }
+            
+            result.push(Vec::with_capacity(8));
+
+            for j in 0..8 {
+                let bit = (byte >> 7 - j) & 1;
+
+                result[i].push(bit);
+            }
+        }
+
+        result
+    }
+
+    pub fn load(raw: &[u8]) -> Result<Font, FontError> {
+        let font = Font {
+            header: Header {
+                magic: [raw[0x0], raw[0x1], raw[0x2], raw[0x3]],
+                version: as_u32_le(&raw[0x4..0x8]),
+                size: as_u32_le(&raw[0x8..0xc]),
+                flags: as_u32_le(&raw[0xc..0x10]),
+                length: as_u32_le(&raw[0x10..0x14]),
+                glyph_size: as_u32_le(&raw[0x14..0x18]),
+                glyph_height: as_u32_le(&raw[0x18..0x1c]),
+                glyph_width: as_u32_le(&raw[0x1c..0x20]),
+            },
+            char_data: (&raw[0x32..]).into(),
+        };
+
+        if font.header.magic != [0x72, 0xb5, 0x4a, 0x86] {
+            return Result::Err(FontError::IncorrectHeader);
+        }
+
+        Result::Ok(font)
+    }
+}
+
+fn as_u32_le(array: &[u8]) -> u32 {
+    ((array[0] as u32) << 0)
+        + ((array[1] as u32) << 8)
+        + ((array[2] as u32) << 16)
+        + ((array[3] as u32) << 24)
+}
--- planes/src/main.rs
+++ planes/src/main.rs
@@ -1,38 +1,46 @@
 use std::{
-    io::{stdin, Read},
-    sync::mpsc::channel,
-    thread, time::{self, Duration},
+    sync::{Arc, Mutex}, fs,
 };
 
-use crossterm::execute;
-use planes::gfx;
+use crossterm::event::KeyCode;
+use planes::gfx::{self, screen::{Screen, RGB}, font::Font, input::on_input};
 
 fn main() {
-    print!("\x1b[?25l");
-
-    let (sender, receiver) = channel::<gfx::ChannelMessage>();
-
-    let screen = gfx::Screen::new(|screen| {
-        screen.bg(gfx::RGB(135, 206, 235));
+    let screen = Screen::new(|screen| {
+        screen.bg(RGB(135, 206, 235));
     });
 
-    let mut i = 0;
-    let plane = gfx::Screen::load_image("./small.png");
+    let pos = Arc::new(Mutex::new((0, 0)));
+    let plane = Screen::load_image("./small.png");
+    let font = Font::load(&fs::read("./font.psfu").unwrap()).unwrap();
 
-    let update = screen.update(receiver, move |screen| {
-        i = i + 1;
-        
-        if i > screen.width - plane[0].len() {
-            i = 0;
-        }
+    let pos_mutex = pos.clone();
+    let (sender, update) = screen.on_update(move |screen| {
+        let pos = *pos_mutex.lock().unwrap();
 
-        // screen.rectangle(i, 0, i+2, 10, gfx::RGB(0, 255, 0));
-        // screen.circle(40, 20, 10, gfx::RGB(0, 0, 255));
-        screen.image(i, 20, &plane, false, true);
+        // screen.text(20, 0, RGB(0, 0, 0), &font, "google en");
+        // screen.text(20, 20, RGB(0, 0, 0), &font, "passant");
+        // screen.image(pos.0, pos.1, &plane, true, false, true);
+        // screen.rectangle(10, 40, 50, 50, RGB(255, 0, 0));
+        // screen.circle(70, 60, 10, RGB(0, 255, 0));
+        //screen.line(0, 0, screen.width-1, screen.height-1, RGB(0, 255, 0));
+        screen.triangle(0, 0, 0, 10, 40, 10, RGB(0, 255, 0))
     });
 
-    stdin().read(&mut [0]).unwrap();
-    sender.send(gfx::ChannelMessage::Stop).unwrap();
+    let pos_mutex = pos.clone();
+    let input = on_input(move |key| {
+        let mut pos = pos_mutex.lock().unwrap();
 
-    print!("\x1b[2J\x1b[H\x1bc\x1b[?25h");
+        match key.code {
+            KeyCode::Right => pos.0 += 1,
+            KeyCode::Left => pos.0 -= 1,
+            KeyCode::Down => pos.1 += 1,
+            KeyCode::Up => pos.1 -= 1,
+            _ => ()
+        };
+    });
+
+    input.join().unwrap();
+    sender.send(gfx::screen::ChannelMessage::Stop).unwrap();
+    update.join().unwrap();
 }
